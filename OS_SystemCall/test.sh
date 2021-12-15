0728da0e12085ec931fa1eacc67544a70c670457
Colorful12 <cat37280@gmail.com>
2021-12-16 05:36:20 +0900

    fix programs related to popup in OS_SystemCall

--- OS_SystemCall/test.sh
+++ OS_SystemCall/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-zenity --info --title="やったね！" --text="ポップアップの表示に成功しました。次に進みましょう。 " --width=500
+zenity --info --title=$1 --text=$2 --width=500
 exit 0
--- OS_SystemCall/test1.c
+++ OS_SystemCall/test1.c
@@ -1,7 +1,15 @@
 #include<stdlib.h>
+#include <stdio.h> /* for snpringf()*/
+
 
 int main(void){
+    int BUFSIZE = 1000;
+    char buf[BUFSIZE];
+    char first[] = "終了通知";
+    char second[] = "課題1の終了時間です";
+    snprintf(buf, BUFSIZE, "bash test.sh %s %s", first, second);
 
-    system("bash test.sh");
+    system(buf);
+
     return 0;
 }
