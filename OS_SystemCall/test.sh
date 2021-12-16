6c4a259ff2a0024d2ab67718e14b9120c54af370
Colorful12 <cat37280@gmail.com>
2021-12-17 01:50:48 +0900

    fix textmessages

--- OS_SystemCall/test.sh
+++ OS_SystemCall/test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-zenity --info --title=$1 --text=$2 --width=500
+zenity --info --title=$1 --text="時間です : $2" --width=500
 exit 0
--- OS_SystemCall/test1.c
+++ OS_SystemCall/test1.c
@@ -2,12 +2,12 @@
 #include <stdio.h> /* for snpringf()*/
 
 
-void test1(void){
+void test1(char task[]){
     int BUFSIZE = 1000;
     char buf[BUFSIZE];
     char first[] = "終了通知";
-    char second[] = "課題1の終了時間です";
-    snprintf(buf, BUFSIZE, "bash test.sh %s %s", first, second);
+
+    snprintf(buf, BUFSIZE, "bash test.sh %s %s", first, task);
 
     system(buf);
 }
